ea2335a99d1efb30531c33515d7d12a47020de48
Cristian Mateos López <mateos.c19@gmail.com>
2021-01-22 12:28:32 +0100

    update vscode

--- vscode/install.sh
+++ vscode/install.sh
@@ -11,7 +11,7 @@ code --install-extension ms-vscode-remote.remote-containers
 # Git.
 code --install-extension eamodio.gitlens
 code --install-extension mhutchie.git-graph
-code --install-extension codezombiech.gitignore	
+code --install-extension codezombiech.gitignore
 
 # Mejora visual del editor.
 code --install-extension coenraads.bracket-pair-colorizer-2
@@ -26,8 +26,9 @@ code --install-extension karigari.chat
 code --install-extension esbenp.prettier-vscode
 code --install-extension christian-kohler.path-intellisense
 code --install-extension formulahendry.auto-close-tag
+code --install-extension editorconfig.editorconfig
 
 # Nueva funcionalidad.
 code --install-extension humao.rest-client
 code --install-extension pnp.polacode
-
+code --install-extension alefragnani.project-manager
